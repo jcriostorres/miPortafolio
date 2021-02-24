f73f83112f3367da4dd3b6a6055436f9cd23ef2e
Johana catalina rios torres <jcriostorres@gmail.com>
2021-02-24 18:34:04 -0500

    modificando template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
+
   ],
   imports: [
     BrowserModule
