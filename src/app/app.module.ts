8ea3c9719ff76d4c49bb0841459b075f49664047
Johana catalina rios torres <jcriostorres@gmail.com>
2021-02-25 20:25:52 -0500

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     
 
   ],
